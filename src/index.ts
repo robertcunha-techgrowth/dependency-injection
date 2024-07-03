e2280a8d788a6e9fa0d6780a1eb739077de2065f
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-07-03 17:50:15 -0300

    refactor: exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./inject";
+export * from "./injectable";
+export * from "./provider";
+export * from "./module";
+export * from "./techgrowth-labs-lambda-handler";
+export * from "./test-module";
