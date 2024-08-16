882ac098a0236669dcb4e6a2a4db3a91dbc15962
Robert Cunha <robert.cunha@techgrowth-labs.com.br>
2024-08-15 23:54:30 -0300

    fix: add module

--- src/techgrowth-labs-lambda-handler.ts
+++ src/techgrowth-labs-lambda-handler.ts
@@ -1,6 +1,8 @@
 import { ModuleMetadata } from "./module-metadata";
 
 export abstract class TechgrowthLabsLambdaHandler {
+	constructor(protected readonly module: any) {}
+
 	abstract handler(event: any, context: any): Promise<any>;
 
 	protected getProviders(moduleName: string) {
